2c11c8fd1b36fa22d52ceb524bfebea65a1fbd04
Baz <61154071+bazmurphy@users.noreply.github.com>
2022-10-25 00:03:04 +0100

    left out by accident from previous commit

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -1,6 +1,9 @@
+// bring in moment js
 const moment = require("moment");
 
 module.exports = {
+  // create a function that takes in a date and a format we specify
+  // and returns moment applied on that date and the moment format of the format we specify
   formatDate: function (date, format) {
     return moment(date).format(format);
   },
